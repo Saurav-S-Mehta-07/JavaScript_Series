04fd30aa78271933d1607f051c685d4ef4c04707
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 09:53:08 +0530

    variable + datatype

---
+++ datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3+3)
+
+console.log("Saurav")
+
+let name = "hitesh"
+let age = 12
+let isLoggedIn = false
+
+// number -> 2 to power 53 (range)
+//bigint
+//string->""
+// boolean -> true/ false
+// null-> standard alone value
+// undefine ->  not assigned yet
+// symbol -> unique
+
+// object jkl
+
+console.log(typeof("saurav"))
+console.log(typeof(null))
+console.log(typeof(undefined))
