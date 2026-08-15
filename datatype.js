b48776bb04c11df73976f96f485295ec7402149c
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 10:31:32 +0530

    datatype summary

--- datatype.js
+++ datatype.js
@@ -2,13 +2,13 @@
 
 // alert(3 + 3) // we are using node.js not browser
 
-console.log(3+3)
+// console.log(3+3)
 
-console.log("Saurav")
+// console.log("Saurav")
 
-let name = "hitesh"
-let age = 12
-let isLoggedIn = false
+// let name = "hitesh"
+// let age = 12
+// let isLoggedIn = false
 
 // number -> 2 to power 53 (range)
 //bigint
@@ -20,6 +20,44 @@ let isLoggedIn = false
 
 // object jkl
 
-console.log(typeof("saurav"))
-console.log(typeof(null))
-console.log(typeof(undefined))
+// console.log(typeof("saurav"))
+// console.log(typeof(null))
+// console.log(typeof(undefined))
+
+
+
+
+// Primitive -> call by value
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+// js is a dynamically typed language
+const score = 100
+const storeValue  = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id == anotherId)
+
+const bigNumber = 34343449058233948394n
+// console.log(typeof(bigNumber))
+
+
+// Reference type (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let obj = {
+    name : "saurav",
+    age : 22,
+}
+
+const myfunction = function(){
+    console.log("Hello world!");
+}
+
+console.table([typeof(heros), typeof(obj), typeof(myfunction)])
