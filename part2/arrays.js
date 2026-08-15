e7a172c4c030621969ea40f3f89ad70d0e19aecd
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 11:54:55 +0530

    arrays done

--- part2/arrays.js
+++ part2/arrays.js
@@ -1,9 +1,9 @@
 // array
 
-const myArr = [0,1,2,3,4,5]
-const myHeros = ["shakriman", "naagraj"]
+// const myArr = [0,1,2,3,4,5]
+// const myHeros = ["shakriman", "naagraj"]
 
-const myArr2 = new Array(1,2,3,4,5)
+// const myArr2 = new Array(1,2,3,4,5)
 
 // console.log(myArr[0])
 
@@ -28,17 +28,51 @@ const myArr2 = new Array(1,2,3,4,5)
 
 // slice, splice
 
-console.log("A ", myArr)
+// console.log("A ", myArr)
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1)
-console.log("B ", myArr)
+// console.log(myn1)
+// console.log("B ", myArr)
 
 
-console.log("A ", myArr)
+// console.log("A ", myArr)
 
-const myn2 = myArr.splice(1, 3)
+// const myn2 = myArr.splice(1, 3)
 
-console.log(myn2)
-console.log("B ", myArr)
+// console.log(myn2)
+// console.log("B ", myArr)
+
+
+
+// *************************************
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3])
+
+
+// const all_heros  = marvel_heros.concat(dc_heros)
+// console.log(all_heros)
+
+const all_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_heros)
+
+
+const another_arr = [1,2,3, [4,5,6], 7,[6,7,[4,5]]]
+
+const real_another_arr = another_arr.flat(Infinity)
+// console.log(real_another_arr)
+
+console.log(Array.isArray("Saurav"))
+console.log(Array.from("Saurav"))
+console.log(Array.from({name : "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
