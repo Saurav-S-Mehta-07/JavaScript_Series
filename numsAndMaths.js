0126f030f29f1a655db306ca8ce904a83890a726
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 11:14:21 +0530

    num and maths

---
+++ numsAndMaths.js
@@ -0,0 +1,46 @@
+const score = 404
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 23.44594
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+inf = Number.MAX_VALUE
+infNeg = Number.MIN_VALUE
+// console.log(inf)
+// console.log(infNeg)
+
+
+
+// ******************** Maths *******************
+//Math module/library by default in js
+
+// console.log(Math)
+
+// console.log(Math.abs(-3))
+// console.log(Math.round(5.43))
+// console.log(Math.floor(5.67))
+// console.log(Math.ceil(5.67))
+// console.log(Math.sqrt(4))
+// console.log(Math.pow(2,3))
+
+// console.log(Math.min(1,2,3,4))
+// console.log(Math.max(1,2,3,4))
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10) + 1)
+
+
+const min = 10
+const max = 20
+
+console.log (Math.floor((Math.random() * (max - min + 1)) + min))
