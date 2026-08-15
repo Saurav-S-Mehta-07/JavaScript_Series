25fe19badbe039ecea96bedfe4d93200a56ef8e1
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 11:43:11 +0530

    array part 1 done

---
+++ part2/arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shakriman", "naagraj"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr[0])
+
+
+// arr method
+
+// myArr.push(6)
+// myArr.push(8)
+// myArr.pop()
+
+// myArr.unshift(10)
+// myArr.shift()
+
+// console.log(myArr.includes(1))
+// console.log(myArr.indexOf(8))
+
+// cosnt = newArr = myArr.join()
+
+// console.log(newArr)
+// console.log(myArr)
+
+
+// slice, splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1)
+console.log("B ", myArr)
+
+
+console.log("A ", myArr)
+
+const myn2 = myArr.splice(1, 3)
+
+console.log(myn2)
+console.log("B ", myArr)
