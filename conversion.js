4b86e39b67e9a868cb4e3a4eefceffb6000fadf3
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-15 10:02:25 +0530

    type conversion

---
+++ conversion.js
@@ -0,0 +1,31 @@
+// let score = "saurav"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// console.log(typeof NaN) // number
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1, false -> 0
+// null -> 0, undefined -> NaN
+// "string" -> NaN
+
+// let isLoggedIn = NaN
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1, "abd" -> true
+// "", Null, undefined, NaN, 0 => false
+
+let num = 33
+let str = String(num)
+console.log(str)
+console.log(typeof str)
+
+
