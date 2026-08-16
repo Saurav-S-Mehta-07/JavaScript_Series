66b9c4c071d17dc1601e345fe8ddefd8c5b5b93a
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-16 16:36:11 +0530

    if else + switch

---
+++ part3/controlflow.js
@@ -0,0 +1,73 @@
+
+// if else
+const temperature = 41
+// if(temperature == 50){
+//    console.log("equal to 50")
+// }
+// else if(temperature<50){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("greater than 50")
+// }
+
+const score = 200
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power : ${power}`)
+// }
+// else{
+//     const power = "walk"
+//     console.log(`User power : ${power}`)
+// }
+
+
+const balance = 344
+
+// if (balance > 500) console.log("test"), console.log("test2"); 
+
+// if(balance < 500 && balance > 250){
+//     console.log("between 500 and 250")
+// }
+// else{
+//     console.log("not in between 500 and 250")
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+
+// if(userLoggedIn && debitCard){
+//     console.log("allow to buy course")
+// }
+// else{
+//     console.log("not allowed")
+// }
+
+function printDay(day){
+    console.log(day)
+}
+
+const day = 9
+
+// switch(day){
+//     case 1 : printDay("Monday")
+//              break;
+//     case 2 : printDay("Tuesday");
+//              break;
+//     case 3 : printDay("Wednesday");
+//              break;
+//     case 4 : printDay("Wednesday");
+//              break;
+//     case 5 : printDay("Wednesday");
+//              break;
+//     case 6 : printDay("Wednesday");
+//             break;
+//     case 7 : printDay("Wednesday");
+//              break;
+//     default : printDay("Invalid");
+// }
+
+
+
