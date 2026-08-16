92d4f976b7bb158022945ec78b974d40fbd6c0ce
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-16 17:11:11 +0530

    while and do while

---
+++ part3/while.js
@@ -0,0 +1,18 @@
+// let i = 0;
+// while(i <= 10){
+//     console.log(i)
+//     i = i+2
+// }
+
+// let arr = ["flash", "batman", "superman"]
+
+// let i = 0;
+// while(i<arr.length){
+//     console.log(arr[i++])
+// }
+
+// let score = 1
+// do{
+//  console.log(score)
+//  score++
+// }while(score <= 5)
