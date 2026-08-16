f8fcc9f0155f3280c37e669e4a5a63837aeeba13
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-16 14:18:16 +0530

    object part 2 completed

--- part2/objects.js
+++ part2/objects.js
@@ -29,13 +29,75 @@ const jsUser = {
 // console.log(jsUser.email)
 
 
-jsUser.greeting = function(){
-    console.log("Hello JS User")
+// jsUser.greeting = function(){
+//     console.log("Hello JS User")
+// }
+
+// jsUser.greetingTo = function(){
+//     console.log(`Hello JS User ${this.name}!`)
+// }
+
+// jsUser.greeting()
+// jsUser.greetingTo()
+
+
+
+// **********************************************
+
+// const tinderUser = new Object() // singleton object
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "dustibun"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "dustibun@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Dustibun",
+            lastname : "Heterson"
+        }
+    }
 }
 
-jsUser.greetingTo = function(){
-    console.log(`Hello JS User ${this.name}!`)
-}
+// console.log(regularUser.fullname.userfullname.firstname)
 
-jsUser.greeting()
-jsUser.greetingTo()
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "a", 4 : "b"}
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id : 1, 
+        email : "a@gmail.com"
+    },
+    {
+        id : 2, 
+        email : "b@gmail.com"
+    },
+    {
+        id : 3, 
+        email : "c@gmail.com"
+    }
+]
+
+// console.log(users[1].email)
+
+
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('name'))
+
+// **************************************************************************
+
+
