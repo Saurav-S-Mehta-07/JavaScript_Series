497c903a8527202dfdfadf7436bc97fcdc93a771
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-16 17:04:54 +0530

    for loop done

---
+++ part3/iteration.js
@@ -0,0 +1,40 @@
+// for
+const array = [1,2,3,4,5]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
+// for(const i in array){
+//     // console.log(i) // here i is index
+//     console.log(array[i])
+// }
+
+// for(let i = 0; i<=10; i++){
+//     if(i==5){
+//         console.log("5 is best number")
+//     }
+//     console.log(i)
+// }
+
+// for(let i = 0; i<=5; i++){
+//     for(let j = 0; j<=5; j++){
+//         console.log(`${i} with ${j}`)
+//     }
+// }
+
+// array.forEach(el =>{
+//     console.log(el)
+// })
+
+
+// for(let i = 0; i <= 5; i++){
+//     if(i==3) break;
+//     console.log(i)
+// }
+
+// for(let i = 0; i<=5; i++){
+//     if(i==3) continue;
+//     console.log(i)
+// }
+
