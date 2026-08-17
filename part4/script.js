91050f2e7c0ae43d7119b91032ea7d7385210758
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-17 13:59:44 +0530

    parent children

--- part4/script.js
+++ part4/script.js
@@ -1,30 +1,21 @@
-let h1 = document.getElementById("title")
-h1.style.color = "red"
-h1.style.border = "2px solid pink"
-h1.style.textAlign = "center"
+const parent = document.querySelector(".parent");
+// console.log(parent)
 
-let h2 = document.querySelector("h2")
-h2.innerText  = "I'm Heading 2"
+// console.log(parent.children)
 
-h2 = document.querySelectorAll("h2")
-h2.forEach(h => {
-    h.innerText = "all heading 2 changed"
-});
 
-pass = document.querySelector("input[type='password']")
+// for(let i = 0; i<parent.children.length; i++){
+//     console.log(parent.children[i].innerText);
+// }
 
-const ul = document.querySelectorAll("li")
-// console.log(ul)
+parent.children[1].style.color = "blue";
 
-ul.forEach((l)=>{
-    l.style.color = "blue"
-})
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
 
-let allList = document.getElementsByClassName("list-item")
-allList = Array.from(allList);
-// console.log(allList)
+const dayOne = document.querySelector(".day")
+console.log(dayOne)
+console.log(dayOne.parentElement)
+console.log(dayOne.nextElementSibling)
 
-allList.forEach((list)=>{
-    console.log(list.innerText)
-})
-
+console.log("Notes : ", parent.childNodes)
