def3152fd4236eb988fbe8546142aedd019381a0
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-22 10:13:51 +0530

    inheritance

---
+++ part7/inheritence.js
@@ -0,0 +1,25 @@
+class User{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username);
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log("A new course was added by ", this.username)
+    }
+}
+
+const t1 = new Teacher("Mayank", "email", "123")
+console.log(t1)
+t1.addCourse()
