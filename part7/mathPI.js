68b30c9e8f0319e4d9c2462830b4f42132f50d46
Saurav s Mehta <sauravmehta631@gmail.com>
2026-08-22 10:56:34 +0530

    writable, enumerable true, false

---
+++ part7/mathPI.js
@@ -0,0 +1,29 @@
+
+// console.log(Math.PI);
+// Math.PI
+// console.log(Math.PI)
+
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+// console.log(descripter)
+
+// const user = {
+//     name : "saurav",
+//     score : 250,
+//     isLoggedIn : true
+// }
+
+// console.log(Object.getOwnPropertyDescriptor(user, "name"))
+
+// Object.defineProperty(user, "name", {
+//     writable : false,
+//     enumerable : false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(user, "name"))
+
+
+// for (let [key, value] of Object.entries(user)) {
+//     if(typeof value !== 'function'){
+//        console.log(`${key} : ${value}`);
+//     }
+// }
